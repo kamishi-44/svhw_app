77b97af0da6d1bfea70cd724ebb540588f941ad1
kamishi-44 <kamishioiri1016@gmail.com>
2022-12-19 00:03:33 +0900

    [add] #3 夏休みの期間のプロバイダーを追加

--- lib/constant/colors.dart
+++ lib/constant/colors.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+/// アプリ内で使用するカラーの定数を定義するクラスです。
 class AppColor {
   /// アプリ内のメインカラー
   static const MaterialColor mainColor = Colors.cyan;
---
+++ lib/constant/constant.dart
@@ -0,0 +1,5 @@
+/// アプリ内で使用する定数を定義します。
+class Constant {
+  /// 月の最後の日(31)
+  static const int lastDay = 31;
+}
