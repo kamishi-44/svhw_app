256739d0cf96b26c0da331a2a830dae0b3075393
kamishi-44 <kamishioiri1016@gmail.com>
2023-05-24 13:14:15 +0900

    [add] #55 宿題の量を登録する StateNotifier, Provider を追加する

--- lib/model/progress.dart
+++ lib/model/progress.dart
@@ -16,6 +16,9 @@ class Progress {
   /// 現在完了した数
   int completed;
 
+  // コンストラクタ
+  /// 先に Homework オブジェクトを生成し、宿題IDが
+  /// 裁判されてから呼び出すようにします。
   Progress(
       {this.id = 0,
       required this.homeworkId,
---
+++ lib/provider/notifier/progress_notifier.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../main.dart';
+import '../../model/progress.dart';
+import '../../objectbox.g.dart';
+
+/// 登録予定宿題の量の StateNotifier です。
+class ProgressNotifier extends StateNotifier<List<Progress>> {
+  ProgressNotifier(): super([]);
+
+  final Box<Progress> progressBox = store.box<Progress>();
+
+  /// 宿題の量を追加します。
+  /// DBへの登録は行いません。
+  void addHomeWork(Progress progress) {
+    state = [...state, progress];
+  }
+
+  /// IDを指定して宿題の量を削除します。
+  void removeProgress(int progressId) {
+    state = [
+      for (final progress in state)
+        if (progress.id != progressId) progress,
+    ];
+  }
+
+  /// ProgressNotifier で管理している宿題の量を登録します。
+  /// 夏休みの宿題登録画面で選択された宿題の量です。
+  void insertHomeworks() {
+    for (var progress in state) {
+      progressBox.put(progress);
+    }
+  }
+}
